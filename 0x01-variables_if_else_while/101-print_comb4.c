9f4cc2dc018139ccdc42ace7428892cbaa499b82
cistenco <stanleyijezie4@gmail.com>
2023-08-14 22:02:42 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,24 +1,25 @@
 #include <stdio.h>
-
-
-/**
- * main - prints all the numbers of base 16 in lowercase.
- *
- * Return: Always 0.
- *
- */
-
+/* more headers goes there */
+/* betty style doc for function main goes there */
 int main(void)
 {
-	int num;
-	char letter;
-
-	for (num = 0; num < 10; num++)
-		putchar((num % 10) + '0');
-
-	for (letter = 'a'; letter <= 'f'; letter++)
-		putchar(letter);
-
-	putchar('\n');
-	return (0);
+int a, b, c;
+for(a = '0'; a <= '9'; a++)
+{
+for(b = '0'; b <= '9'; b++)
+{
+for(c = '0'; c <= '9'; c++)
+{
+ if (a < b && b < c)
+ {
+     putchar(a);
+     putchar(b);
+     putchar(c);
+ putchar(',');
+ putchar(' ');
+ }
+}
+}
+}
+    return (0);
 }
