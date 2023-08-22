c276528abce470b55dc662974bd4a8c7bc0880c4
cistenco <stanleyijezie4@gmail.com>
2023-08-21 21:29:19 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char *str;
+
+    str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+    _puts(str);
+    return (0);
+}
+julien@ubuntu:~/0x05$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 _putchar.c 3-main.c 3-puts.c -o 3-puts
+julien@ubuntu:~/0x05$ ./3-puts 
+I do not fear computers. I fear the lack of them - Isaac Asimov
+julien@ubuntu:~/0x05$
